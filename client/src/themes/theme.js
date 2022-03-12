7d1f761429fe6f709952414a1750cf0eb841d7f4
Thomas L'Anglais <thomas1446@gmail.com>
2022-03-11 20:57:01 -0500

    Read status UI Components

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -20,5 +20,22 @@ export const theme = createMuiTheme({
   palette: {
     primary: { main: "#3A8DFF" },
     secondary: { main: "#B0B0B0" }
-  }
+  },
+  chat: {
+    message: {
+      unread: {
+        backgroundColor: '#3F92FF',
+        color: 'white',
+        fontWeight: 'bold',
+      },
+      sender: {
+        background: '#F4F6FA',
+        borderRadius: '10px 10px 0 10px',
+      },
+      other: {
+        backgroundImage: 'linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)',
+        borderRadius: '0 10px 10px 10px',
+      },
+    },
+  },
 });
